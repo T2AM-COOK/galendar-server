847034275b9c6094b7bf47e2507b45ebc935108f
bestdevmgp <pmg3858@icloud.com>
2024-10-01 23:17:09 +0900

    fix: JwtFilter

--- src/main/java/com/k/garlander/jwt/JwtFilter.java
+++ src/main/java/com/k/garlander/jwt/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -24,9 +26,8 @@ public class JwtFilter extends OncePerRequestFilter {
 
         String authorization = request.getHeader("Authorization");
 
-        if(authorization == null || !authorization.startsWith("Bearer")){
+        if (authorization == null || !authorization.startsWith("Bearer")){
             filterChain.doFilter(request,response);
-
             return;
         }
 
@@ -44,7 +45,7 @@ public class JwtFilter extends OncePerRequestFilter {
         UserEntity userEntity = new UserEntity();
         userEntity.setUserName(userName);
         userEntity.setRole(role);
-        userEntity.setPassWord("temppassword");
+        userEntity.setPassword("tempPassword");
 
         CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
 
