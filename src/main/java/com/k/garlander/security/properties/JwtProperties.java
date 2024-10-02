ffd7216b2654075f311193519db86914b264f4de
bestdevmgp <pmg3858@icloud.com>
2024-10-02 12:20:36 +0900

    add feat: JwtProperties

---
+++ src/main/java/com/k/garlander/security/properties/JwtProperties.java
@@ -0,0 +1,23 @@
+package com.k.garlander.security.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "spring.jwt")
+public class JwtProperties {
+    private String secret;
+    private String access;
+    private String refresh;
+
+    @Setter
+    @Getter
+    public static class Expiration {
+        private long expiration;
+    }
+
+}
