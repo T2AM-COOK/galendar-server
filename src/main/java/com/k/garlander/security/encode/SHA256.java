9d73fda0400cf3de79e2baff6ee02d5cf7b98c02
bestdevmgp <pmg3858@icloud.com>
2024-10-21 20:21:33 +0900

    fix: jwt system

--- src/main/java/com/k/garlander/security/encode/SHA256.java
+++ src/main/java/com/k/garlander/security/encode/SHA256.java
@@ -1,8 +1,11 @@
 package com.k.garlander.security.encode;
 
+import org.springframework.stereotype.Component;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@Component
 public class SHA256 {
     public String encode(String input) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
