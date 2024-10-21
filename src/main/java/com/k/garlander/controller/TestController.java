057a6d5f7aeb1c06146783011094e9f7d1c6f0b2
bestdevmgp <pmg3858@icloud.com>
2024-10-21 20:45:57 +0900

    feat: TestController

---
+++ src/main/java/com/k/garlander/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.k.garlander.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Test", description = "테스트용 API")
+public class TestController {
+    @Operation(
+            summary = "서버 접속 테스트용 코드입니다.",
+            description = "get 이며 token 은 필요없습니다."
+    )
+    @GetMapping("/test")
+    public String test() {
+        return "Galander Server Test";
+    }
+}
