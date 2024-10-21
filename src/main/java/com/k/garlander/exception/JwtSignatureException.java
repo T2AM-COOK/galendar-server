927d7a77343ec2c3e8b24e1d7fd333352bf6ed66
bestdevmgp <pmg3858@icloud.com>
2024-10-21 20:43:37 +0900

    fix: exception handler

--- src/main/java/com/k/garlander/exception/JwtSignatureException.java
+++ src/main/java/com/k/garlander/exception/JwtSignatureException.java
@@ -2,21 +2,16 @@ package com.k.garlander.exception;
 
 public class JwtSignatureException extends RuntimeException {
     public JwtSignatureException(String message) {
-
         super(message);
     }
     public JwtSignatureException(String message, Throwable cause) {
-
         super(message, cause);
     }
 
     public JwtSignatureException(Throwable cause) {
-
         super(cause);
     }
     public JwtSignatureException() {
-
         super();
     }
 }
-
