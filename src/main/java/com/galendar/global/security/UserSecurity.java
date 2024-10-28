e190814fac3b578d469cc2c4b8269f1775d8c5b2
Kim-Wongi-1 <120900266+Kim-Wongi-1@users.noreply.github.com>
2024-10-28 14:45:18 +0900

    feat :: SecurityContextHolder

---
+++ src/main/java/com/galendar/global/security/UserSecurity.java
@@ -0,0 +1,27 @@
+package com.galendar.global.security;
+
+import com.galendar.domain.user.dto.User;
+import com.galendar.domain.user.entity.UserEntity;
+import com.galendar.domain.user.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSecurity {
+
+    public User getUser() {
+        return ((CustomUserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal())
+                .getUser();
+    }
+
+    public UserEntity getUserEntity() {
+        return UserEntity.builder()
+                .id(getUser().getId())
+                .build();
+    }
+
+}
