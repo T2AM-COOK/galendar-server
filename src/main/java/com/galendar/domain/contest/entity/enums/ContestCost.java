e90cdca7548094ca475b229f2096b6c6cd157986
Kim-Wongi-1 <120900266+Kim-Wongi-1@users.noreply.github.com>
2024-10-28 14:54:26 +0900

    feat :: 대회 대상, 지역 다대다 관계

--- src/main/java/com/galendar/domain/contest/entity/enums/ContestCost.java
+++ src/main/java/com/galendar/domain/contest/entity/enums/ContestCost.java
@@ -1,6 +1,12 @@
 package com.galendar.domain.contest.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ContestCost {
-    FREE,
-    PAID
+    FREE("FREE"),
+    PAID("PAID");
+    private final String key;
 }
