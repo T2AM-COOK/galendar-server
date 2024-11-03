f7452d62136fdb21130d289f0c29ee6c1cf3a233
Kim-Wongi-1 <120900266+Kim-Wongi-1@users.noreply.github.com>
2024-11-04 04:30:24 +0900

    feat :: Contest Scheduler

--- src/main/java/com/galendar/GarlanderApplication.java
+++ src/main/java/com/galendar/GarlanderApplication.java
@@ -3,9 +3,11 @@ package com.galendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class GarlanderApplication {
     public static void main(String[] args) {
         SpringApplication.run(GarlanderApplication.class, args);
