5784d13d28636a7189a9653ade4bf6b26a3daa3c
Kim-Wongi-1 <120900266+Kim-Wongi-1@users.noreply.github.com>
2024-11-06 14:30:07 +0900

    feat :: zero-downtime deployment

---
+++ src/main/java/com/galendar/domain/profile/controller/ProfileController.java
@@ -0,0 +1,25 @@
+package com.galendar.domain.profile.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+    private final Environment environment;
+
+    @GetMapping("/profile")
+    public String profile(){
+        List<String> profiles = Arrays.stream(environment.getActiveProfiles()).toList();
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+    }
+
+}
