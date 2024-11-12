7dd00b83c3ee124eda7d46a1e2b98e9b8b072029
bestdevmgp <pmg3858@icloud.com>
2024-11-12 17:44:57 +0900

    feat: BookmarkId

--- src/main/java/com/galendar/domain/bookmark/entity/BookmarkId.java
+++ src/main/java/com/galendar/domain/bookmark/entity/BookmarkId.java
@@ -1,4 +1,29 @@
 package com.galendar.domain.bookmark.entity;
 
-public class BookmarkId {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class BookmarkId implements Serializable {
+    private Long user;
+    private Long contest;
+
+    public BookmarkId() {}
+
+    public BookmarkId(Long user, Long contest) {
+        this.user = user;
+        this.contest = contest;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookmarkId that = (BookmarkId) o;
+        return Objects.equals(user, that.user) && Objects.equals(contest, that.contest);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, contest);
+    }
 }
