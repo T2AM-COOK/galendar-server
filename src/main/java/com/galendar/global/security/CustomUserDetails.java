bdf8265a7d5c8207e9d301363512ab96c7deacd3
bestdevmgp <pmg3858@icloud.com>
2024-11-14 20:06:30 +0900

    fix: CustomUserDetails

--- src/main/java/com/galendar/global/security/CustomUserDetails.java
+++ src/main/java/com/galendar/global/security/CustomUserDetails.java
@@ -5,9 +5,79 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
+//@Getter
+//public class CustomUserDetails implements UserDetails {
+//
+//    private final User user;
+//    private Collection<? extends GrantedAuthority> authorities;
+//
+//    private CustomUserDetails(final User user, final Collection<? extends GrantedAuthority> authorities) {
+//        this.user = user;
+//        this.authorities = authorities;
+//    }
+//
+//    public static CustomUserDetails create(User user) {
+//        return new CustomUserDetails(user, Collections.singleton((GrantedAuthority) user.getRole()::getKey));
+//    }
+//
+////    @Override
+////    public Collection<? extends GrantedAuthority> getAuthorities() {
+////        return authorities;
+////    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//
+//        return authorities;
+//
+//        Collection<GrantedAuthority> collection = new ArrayList<>();
+//
+//        collection.add(new GrantedAuthority() {
+//
+//            @Override
+//            public String getAuthority() {
+//
+//                return userEntity.getRole().value();
+//            }
+//        });
+//
+//        return collection;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getEmail();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
 @Getter
 public class CustomUserDetails implements UserDetails {
 
@@ -25,7 +95,16 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole().name();
+            }
+        });
+
+        return collection;
     }
 
     @Override
