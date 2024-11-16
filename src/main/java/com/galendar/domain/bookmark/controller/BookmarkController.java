a7f44799c2c746e508fd35e70085a68cd1b460a6
bestdevmgp <pmg3858@icloud.com>
2024-11-16 22:01:52 +0900

    fix: BookmarkController

--- src/main/java/com/galendar/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/com/galendar/domain/bookmark/controller/BookmarkController.java
@@ -6,6 +6,7 @@ import com.galendar.domain.bookmark.dto.response.BookmarkResponse;
 import com.galendar.domain.bookmark.service.BookmarkService;
 import com.galendar.domain.bookmark.service.querydsl.BookmarkQueryService;
 import com.galendar.global.common.dto.response.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,32 +19,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name = "북마크", description = "북마크 api입니다.")
 @RestController
 @RequestMapping("/bookmark")
 @RequiredArgsConstructor
+@Tag(name = "북마크", description = "북마크 api 입니다.")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
     private final BookmarkQueryService bookmarkQueryService;
 
     @PostMapping("/{contestId}")
-    public ResponseEntity register(@PathVariable("contestId") Long contesetId) {
-        bookmarkService.register(contesetId);
+    @Operation(
+            summary = "특정 대회에 대한 북마크를 추가합니다.",
+            description = "북마크하고자 하는 대회의 아이디를 경로로 전달합니다."
+    )
+    public ResponseEntity register(@PathVariable("contestId") Long contestId) {
+        bookmarkService.register(contestId);
         return ResponseEntity.ok(ResponseData.ok("북마크가 등록되었습니다."));
     }
 
     @DeleteMapping("/{bookmarkId}")
+    @Operation(
+            summary = "특정 대회에 대한 북마크를 삭제합니다.",
+            description = "북마크 삭제하고자 하는 대회의 아이디를 경로로 전달합니다."
+    )
     public ResponseEntity remove(@PathVariable("bookmarkId") Long bookmarkId) {
         bookmarkService.remove(bookmarkId);
         return ResponseEntity.ok(ResponseData.ok("북마크가 해제되었습니다."));
     }
 
     @GetMapping("/list")
+    @Operation(
+            summary = "대회를 불러옵니다.",
+            description = "인자는 없습니다."
+    )
     public ResponseEntity list(BookmarkRequest request) {
         List<BookmarkResponse> result = bookmarkQueryService.list(request);
         return ResponseEntity.ok(ResponseData.ok(result, "조회 성공"));
     }
-
-
 }
