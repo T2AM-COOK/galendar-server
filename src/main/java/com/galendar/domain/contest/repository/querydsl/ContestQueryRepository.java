2e51eba94df17748318519abe06c70e0d18c95a2
ENCAIL <siwon0876@gmail.com>
2024-11-27 20:21:06 +0900

    fix: QueryRepository

--- src/main/java/com/galendar/domain/contest/repository/querydsl/ContestQueryRepository.java
+++ src/main/java/com/galendar/domain/contest/repository/querydsl/ContestQueryRepository.java
@@ -3,11 +3,15 @@ package com.galendar.domain.contest.repository.querydsl;
 import com.galendar.domain.contest.dto.request.ContestRequest;
 import com.galendar.domain.contest.dto.response.ContestDetailResponse;
 import com.galendar.domain.contest.dto.response.ContestResponse;
+import com.galendar.domain.contest.entity.ContestEntity;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface ContestQueryRepository {
+    List<ContestEntity> findContestsBySubmitEndDates(List<LocalDate> deadlineDates);
+
     List<ContestResponse> find(ContestRequest request);
 
     Optional<ContestDetailResponse> findById(Long id);
